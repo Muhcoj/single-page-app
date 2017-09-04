66e07b1f9652a684cbaad31846ca80bce12fb2a9
Muhcoj <jochum.de.bueger@gmail.com>
2017-09-04 20:31:39 +0200

    Implemented creation process for galleries and refactored

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,4 +1,34 @@
 class GalleriesController < ApplicationController
+	before_action :set_gallery, only: [:show]
 	def index
 	end
+
+	def new
+		@gallery = Gallery.new
+	end
+
+	def create
+		@gallery = Gallery.new(gallery_params)
+
+		if @gallery.save
+			redirect_to @gallery, notice: 'Your update was successful'
+		else
+			render :new
+		end
+	end
+
+	def show
+	end
+
+
+
+	private
+
+	  def gallery_params
+	    params.require(:gallery).permit(:title, :image)
+	  end
+
+	  def set_gallery
+	  	@gallery = Gallery.find(params[:id])
+	  end
 end
--- spec/features/gallery_spec.rb
+++ spec/features/gallery_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'navigation' do
+describe 'navigate' do
 	describe 'index' do 
 		it 'can be reached successfully' do
 			visit galleries_path
@@ -10,6 +10,23 @@ describe 'navigation' do
 		it 'has an image' do
 			visit galleries_path
 			expect(page.find('#image_gallery')['src']).to have_content 'default.png'
+		end
+	end
+
+	describe 'creation' do 
+		before do
+			visit new_gallery_path
+		end
+		it 'has a new form that can be reached' do
+			expect(page.status_code).to eq(200)
+		end
+
+		it 'can be created from new form page' do
+			fill_in 'gallery[title]',  with: 'Some Title'
+			fill_in 'gallery[image]', with: 'default.png'
+			click_on 'Save'
+
+			expect(page).to have_content('Some Title')
 		end
 	end
 end
